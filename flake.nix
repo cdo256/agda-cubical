c6f96ee8576cd10167d84056878b619c3daca358
Christina O'Donnell <cdo@mutix.org>
2025-11-04 04:31:41 +0000

    Remove just-agda from flake.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    just-agda.url = "github:cdo256/just-agda";
   };
 
   outputs =
@@ -17,18 +16,11 @@
         { pkgs, ... }:
         let
           agda = pkgs.agda.withPackages (ps: [ ]);
-          just-agda-base = inputs.just-agda.packages.${pkgs.system}.default;
-          just-agda = just-agda-base.override { inherit agda; };
         in
         {
-          packages = {
-            inherit agda just-agda;
-            default = just-agda;
-          };
           devShells.default = pkgs.mkShell {
             buildInputs = [
               agda
-              just-agda
               pkgs.ghc
               pkgs.gnumake
             ];
